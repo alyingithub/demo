3d6def83e4af11f513c54941617c4400771db006
a1 <alyin8686@gmail.com>
2023-05-19 14:48:20 -0400

    current

---
+++ src/hello/First.java
@@ -0,0 +1,18 @@
+package hello;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+		// this is an edited version
+		System.out.println("im in to something");		// this is an edited version
+
+		System.out.println("but it looks like ");
+		System.out.println(27 * 59);/*this is an edited version*/
+	
+
+
+	}
+
+}
